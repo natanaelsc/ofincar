ecbcec93957abba24baba8f89fc9b4ce796cfe05
Natanael Silva <53065432+natanaelsc96@users.noreply.github.com>
2022-09-10 15:50:23 -0300

    chore: update code formatting

--- src/main/java/br/com/ofincar/controllers/HomeController.java
+++ src/main/java/br/com/ofincar/controllers/HomeController.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    ModelAndView getPage() { return new ModelAndView("index"); }
+    ModelAndView getPage() {
+        return new ModelAndView("index");
+    }
 
 }
